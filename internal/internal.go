8c58b9c4c4f6e90fae47b03269951bbdef315a85
samar2170 <scipioresearch2020@gmail.com>
2023-08-08 22:17:27 +0530

    half done

--- internal/internal.go
+++ internal/internal.go
@@ -6,23 +6,23 @@ import (
 	"github.com/samar2170/portfolio-manager-v4/client/cognitio/cauth"
 	"github.com/spf13/viper"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 )
 
 var passwordDecryptionKey string
-var cognitioClient *cauth.AuthServiceClient
+var cognitioClient = new(cauth.AuthServiceClient)
 
 func init() {
 	viper.SetConfigFile(".env")
 	viper.ReadInConfig()
-	passwordDecryptionKey = viper.GetString("passwordDecryptionKey")
+	passwordDecryptionKey = viper.GetString("PASSWORD_ENCRYPTION_KEY")
+	grpcHost, grpcPort := viper.GetString("GRPC_HOST"), viper.GetString("GRPC_PORT")
 
-	conn, err := grpc.Dial("localhost:9000", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.Dial(grpcHost+":"+grpcPort, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("did not connect: %s", err)
 	}
 	defer conn.Close()
 
-	// cognitioClient = &cauth.NewAuthServiceClient(conn)
+	*cognitioClient = cauth.NewAuthServiceClient(conn)
 
 }
